061106ecb308bea34b659aa68d78c44583ef5323
Walter V <waltervargas@linux.com>
2020-04-11 11:56:06 +0100

    Add functions for datatypes

--- chapter2/src/Chapter2/DataTypes.hs
+++ chapter2/src/Chapter2/DataTypes.hs
@@ -2,11 +2,32 @@ module Chapter2.DataTypes where
 
 data Client = GovOrg String
             | Company    String Integer String String
-            | Individual Person Gender
+            | Individual Person
             deriving Show
 
-data Person = Person String String
+data Person = Person String String Gender
             deriving Show
 
 data Gender = Male | Female | Unknown
             deriving Show
+
+data TimeMachine = TimeMachine String Integer String Time Float
+                 deriving Show
+
+data Time = Past | Future
+          deriving Show
+
+
+companyName :: Client -> Maybe String
+companyName client = case client of
+                       Company name _ _ _ -> Just name
+                       _                  -> Nothing
+
+clientName :: Client -> String
+clientName (GovOrg name)                     = name
+clientName (Company name _ _ _)              = name
+clientName (Individual (Person fNm lNm _)) = fNm ++ " " ++ lNm
+
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n-1) + fibonacci (n-2)
